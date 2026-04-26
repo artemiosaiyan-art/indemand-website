cea2cf337e067b7fd6e6c0d65963b06d5ae69f16
artemiosaiyan-art <artemio.saiyan@gmail.com>
2026-04-25 21:15:22 -0400

    Update layout.tsx

--- layout.tsx
+++ layout.tsx
@@ -1,14 +1,12 @@
 import './globals.css';
 import type { Metadata } from 'next';
 
-export const metadata: Metadata = {
-  title: 'Indemand Building Services',
-  description: 'Electrical, HVAC, roofing, plumbing, cleaning, concrete, asphalt and general repair services in Ontario.',
-};
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-site-verification" content="GgMim3xUOhnfFQeG4QbB6mPJovQCKegAoILd-9NWjrk" />
+      </head>
       <body>{children}</body>
     </html>
   );
